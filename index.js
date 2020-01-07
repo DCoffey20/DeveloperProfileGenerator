c900ec3b19eea9c1caab7b6da7dad552bb3b54a0
Dylan Coffey <dcoffey20@gmail.com>
2020-01-06 20:34:16 -0600

    set up index with the required calls and created the question prompts

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const questions = [
-  
-];
+const html = require('./generateHTML');
+const fs = require('fs'),
+    convertFactory = require('electron-html-to');
+    
+const conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
+});
+const inquirer = require('inquirer');
+const axios = require('axios');
+const util = require('util');
+const asyncWriteFile = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
- 
+const questions = () => {
+    return inquirer.prompt([{
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },
+    {
+        type: "list",
+        name: "color",
+        choices: ["green", "blue", "pink", "red"],
+        message: "Pick your color theme from the following options."
+    }])
 }
-
-function init() {
-
-init();
